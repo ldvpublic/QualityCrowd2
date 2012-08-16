d44be2d5d5a19ec538c12fd6d55499190645b231
Clemens Horch <ch@tum.de>
2012-08-16 09:33:57 +0200

    moved markdown to submodule

--- core/lib/AdminDoc.php
+++ core/lib/AdminDoc.php
@@ -4,7 +4,7 @@ class AdminDoc extends AdminPage
 {
 	protected function prepareRender()
 	{
-		require(ROOT_PATH . 'core'.DS.'3p'.DS.'markdown.php');
+		require(ROOT_PATH . 'core'.DS.'3p'.DS.'markdown'.DS.'markdown.php');
 		$md = file_get_contents(ROOT_PATH . 'core'.DS.'doc'.DS.'qc-script.md');
 		$html = Markdown($md);
 		$this->tpl->set('doc', $html);
